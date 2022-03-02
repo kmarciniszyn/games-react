8752d42b655b2af3525dcfa52c81c0f309d249e2
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-03-02 14:01:37 +0100

    fix 'createRandomMinedBoard' function

--- src/game/createRandomMinedBoard.js
+++ src/game/createRandomMinedBoard.js
@@ -1,8 +1,7 @@
 export const createRandomMinedBoard = (size, minesAmount) => {
     const minedBoard = []; // "x" means this position is mined
 
-    const xMinesCoordinates = getRandomCoordinates(size, minesAmount);
-    const yMinesCoordinates = getRandomCoordinates(size, minesAmount);
+    const minesCoordinates = getRandomCoordinates(size, minesAmount);
 
     for (let y = 0; y < size; y++) {
         const row = [];
@@ -15,16 +14,29 @@ export const createRandomMinedBoard = (size, minesAmount) => {
     }
 
     for (let i = 0; i < minesAmount; i++) {
-        minedBoard[xMinesCoordinates[i]][yMinesCoordinates[i]] = ("x");
+        minedBoard[minesCoordinates[i].x][minesCoordinates[i].y] = ("x");
     }
 
     return minedBoard;
 };
 
 const getRandomCoordinates = (coordinatesNumber, minesAmount) => {
-    const coordinates = new Set();
-    while (coordinates.size < minesAmount) {
-        coordinates.add(Math.floor(Math.random() * coordinatesNumber));
+    const coordinates = [];
+
+    while (coordinates.length < minesAmount) {
+        const x = Math.floor(Math.random() * coordinatesNumber);
+        const y = Math.floor(Math.random() * coordinatesNumber);
+
+        const randomCoordinates = {
+            id: `x${x}y${y}`,
+            x: x,
+            y: y,
+        };
+
+        if(coordinates.findIndex(item => item.id === randomCoordinates.id) === -1){
+            coordinates.push(randomCoordinates);
+        }
     }
-    return Array.from(coordinates);
+
+    return coordinates;
 };
