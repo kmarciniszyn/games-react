ad5c5d82e6ea49c0d866ff91cd7173046cbc4812
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-03-03 20:22:34 +0100

    creatimg useRevealCell custom hook

---
+++ src/minesweeper/MinesweeperPage/Game/useRevealCell.js
@@ -0,0 +1,73 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectBoardSize,
+    selectStateBoard,
+    updateStateBoard,
+    endGame
+} from "../../minesweeperSlice";
+
+const useRevealCell = () => {
+    const dispatch = useDispatch();
+    const boardSize = useSelector(selectBoardSize);
+    const stateBoard = useSelector(selectStateBoard);
+
+    const markNearlyZero = (x, y, revealedCells) => {
+
+        revealedCells = markIfZero(x + 1, y + 1, revealedCells);
+        revealedCells = markIfZero(x + 1, y, revealedCells);
+        revealedCells = markIfZero(x + 1, y - 1, revealedCells);
+        revealedCells = markIfZero(x, y - 1, revealedCells);
+        revealedCells = markIfZero(x, y + 1, revealedCells);
+        revealedCells = markIfZero(x - 1, y + 1, revealedCells);
+        revealedCells = markIfZero(x - 1, y, revealedCells);
+        revealedCells = markIfZero(x - 1, y - 1, revealedCells);
+
+        return revealedCells;
+    };
+
+    const markIfZero = (x, y, revealedCells) => {
+        if (x < 0 || y < 0 || x >= boardSize || y >= boardSize) return revealedCells;
+
+        const id = `x${x}y${y}`;
+        const index = stateBoard.findIndex(item => item.id === id);
+
+        if (revealedCells.includes(id)) return revealedCells;
+
+        if (stateBoard[index].clicked === true) return revealedCells;
+
+        if (stateBoard[index].text === 0) {
+            dispatch(updateStateBoard(id));
+            revealedCells.push(id);
+            revealedCells = markNearlyZero(x, y, revealedCells);
+
+            return revealedCells;
+
+        } else if (stateBoard[index].text === "x") {
+            return revealedCells;
+
+        } else {
+            dispatch(updateStateBoard(id));
+            revealedCells.push(id);
+
+            return revealedCells;
+        }
+    };
+
+    return (id, x, y) => {
+        const index = stateBoard.findIndex(item => item.id === id);
+
+        if (stateBoard[index].text === 0) {
+            const revealedCells = [];
+            dispatch(updateStateBoard(id));
+            markNearlyZero(x, y, revealedCells);
+
+        } else if (stateBoard[index].text === "x") {
+            dispatch(endGame());
+
+        } else {
+            dispatch(updateStateBoard(id));
+        }
+    };
+};
+
+export default useRevealCell;
